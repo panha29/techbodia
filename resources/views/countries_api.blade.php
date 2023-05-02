7681ca274ff01ccb431a57efdec81b1173a1b8fd
panha29 <78394799+panha29@users.noreply.github.com>
2023-05-02 11:49:08 +0700

    update

--- resources/views/countries_api.blade.php
+++ resources/views/countries_api.blade.php
@@ -9,28 +9,31 @@ const loadCountryAPI = () =>{
 
 // displaying all countries
 const displayCountries = countries =>{
-    // console.log(countries);
+
     const countriesHTML = countries.map(country => getCountry(country));
-    // displaying div to html
     const container = document.getElementById('countries');
     container.innerHTML = countriesHTML.join(' ');
+    // console.log(countries);
+
 
 }
 
 // get data and set it to html
 const getCountry = (country) =>{
-    return `
-        <tr id="">
-            <td>${country.name.official}</td>
-            <td><img src="${country.flags.png}" style="width:75px"></td>
-            <td>${country.cca2}</td>
-            <td>${country.cca3}</td>
-            <td>${country.name.common}</td>
-            <td>${country.altSpellings}</td>
-            <td>${country.idd.root}</td>
-        </tr>
+    console.log(country.name.nativeName.khm);
 
+    return `
+    <tr id="">
+     <td>${country.name.official}</td>
+     <td><img src="${country.flags.png}" style="width:75px"></td>
+     <td>${country.cca2}</td>
+     <td>${country.cca3}</td>
+     <td>${country.name.nativeName}</td>
+     <td>${country.altSpellings}</td>
+     <td>${country.idd.root}</td>
+ </tr>
     `
+
 }
 // call the funtion to get output in console
 loadCountryAPI()
